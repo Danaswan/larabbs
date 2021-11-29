0149929f802861c9a2ac26637952ace9ed873833
dana <1263038804@qq.com>
2021-11-29 14:05:54 +0800

    '用户个人页面原型'

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -48,3 +48,6 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+// User
+Route::resource('users', 'UserController', ['only' =>'show', 'update', 'edit']);
